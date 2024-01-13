c00be6a9ae45148d3369b071f737abf7efca06d3
Kushagra <you@example.com>
2024-01-13 10:13:35 -0500

    added 2 items so i could do back page

--- stuff/percentCalculator_forMoney.py
+++ stuff/percentCalculator_forMoney.py
@@ -1,16 +1,20 @@
+open("answers.txt", "w").close()
+
 HST = 13
 
 numbers = input('Questions : ')
-questionsList = list(map(float, numbers.split(',')))
+pairs = numbers.split(',')
+questionsList = {float(pair[0]): float(pair[1]) for pair in zip(pairs[::2], pairs[1::2])}
 
 HSTorNot = input('Do you want to calculate tax?(13%)? ')
 
-def percentage(*percentAndAmount):
-    percentAndAmount = list(percentAndAmount)
-    return (percentAndAmount[0] * percentAndAmount[1]) / 100
-
-for number in questionsList:
-    Tax = percentage(HST, number)
-    roundedTax = round(Tax, 2)
-    roundedNumber = round(roundedTax + number, 2)
-    print(f' Tax : {roundedTax} \n Total with tax : {roundedNumber}')
+def percentage(percent, amount):
+    return (percent * amount) / 100
+answers = []
+for item1_Price, item2_Price in questionsList.items():
+    totalPrice = item1_Price + item2_Price
+    tax = round(percentage(HST, totalPrice), 2)
+    totalWithTax = round(totalPrice + tax, 2)
+    # change = round(moneyUsed - totalWithTax, 2)
+    with open("answers.txt", "a") as file:
+       file.write(f'Subtotal : {totalPrice} | Taxes : {tax} | Total Cost : {totalWithTax}\n')
